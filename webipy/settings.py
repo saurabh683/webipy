73d564d9565ccbd1bda54954459c23186810f50e
saurabh683 <saurabhqa683@gmail.com>
2017-08-15 23:32:27 +0530

    Update settings.py

--- webipy/settings.py
+++ webipy/settings.py
@@ -7,7 +7,7 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+DISABLE_COLLECTSTATIC=1
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
